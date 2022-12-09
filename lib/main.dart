4508f1ce35abc2aa427b12adf9cc314710c2f75c
chusamasadiq <usamasadiq163@gmail.com>
2022-12-09 09:30:49 +0500

    Webview

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const WebView());
+
+class WebView extends StatelessWidget {
+  const WebView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
